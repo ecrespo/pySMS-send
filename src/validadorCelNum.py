4f860782737cef8ec079cae8e4823ede7ccb7d49
Ernesto Crespo <ecrespo@gmail.com>
2011-06-15 08:55:43 -0530

    agregada documentacion al modulo validadorCelNum

--- src/validadorCelNum.py
+++ src/validadorCelNum.py
@@ -12,6 +12,10 @@ Version: 0.2
 
 import re
 def Validar(numero):
+    """
+    Devuelve 1 si el numero es valido,
+    devuelve 0 si el numero no es valido.
+    """
     #Valida si los numeros tienen 11 digitos y que sean de los proveedores movilnet, digitel y movistar
     if len(numero) == 11 and ((re.search("041[2|4|6]\d\d\d\d\d\d\d",numero)) or (re.search("042[4|6]\d\d\d\d\d\d\d",numero))) :
         return 1
